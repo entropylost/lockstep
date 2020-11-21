08e3d9b94a1f6d46f37337b3ce83814a085c60c8
iMplode nZ <rg@youxplode.com>
2020-11-21 12:37:23 -0800

    Finished latency.

--- src/latencySettingsRenderer.js
+++ src/latencySettingsRenderer.js
@@ -5,39 +5,71 @@ const { mod } = require('./settings');
 function rand(center, dist) {
     return Math.random() * 2 * dist + center - dist;
 }
-const { RENDER_VIEWPORT, RENDER_SCALE } = require('./constants');
+const { RENDER_VIEWPORT, RENDER_SCALE, LATENCY_MAX } = require('./constants');
 const width = RENDER_VIEWPORT.width * RENDER_SCALE;
-const scale = width / 1000;
+const scale = width / LATENCY_MAX;
 module.exports = function latencySettings(ty) {
-    const center = rand(500, 250);
-    const dist = Math.random() * 250 + 40;
+    let center = rand(500, 250);
+    let dist = Math.random() * 250;
     const latency = () => rand(center, dist);
-    console.log(`Creating settings: ${center}, ${dist}`);
+    let dragged = null;
+    let lastMousePosition = null;
+    document.addEventListener('mousemove', (e) => {
+        if (lastMousePosition != null) {
+            const delta = (e.clientX - lastMousePosition) / scale;
+            if (dragged == 'left') {
+                dist -= delta;
+            } else if (dragged == 'center') {
+                center += delta;
+            } else if (dragged == 'right') {
+                dist += delta;
+            }
+            dist = Math.max(dist, 0);
+            center = Math.min(Math.max(center, dist + 60), LATENCY_MAX - dist - 60);
+        }
+        lastMousePosition = e.clientX;
+    });
+    document.addEventListener('mouseup', () => (dragged = null));
     const component = mod(`latency${ty}`, (css, use, $) => {
         css(require('./latency.scss'));
         return {
             view() {
-                return $._.box(
-                    {
-                        style: {
-                            __width: width,
+                return $._.container(
+                    $._.box(
+                        {
+                            style: {
+                                __width: width,
+                            },
                         },
-                    },
-                    $._.centerMark({
-                        style: {
-                            __pos: center * scale,
-                        },
-                    }),
-                    $._.left({
-                        style: {
-                            __pos: (center - dist) * scale,
-                        },
-                    }),
-                    $._.right({
-                        style: {
-                            __pos: (center + dist) * scale,
-                        },
-                    })
+                        $._.text(`${ty}put lag controls`),
+                        $._.centerMark({
+                            style: {
+                                __pos: center * scale,
+                            },
+                            onmousedown() {
+                                dragged = 'center';
+                                return false;
+                            },
+                        }),
+                        $._.left({
+                            style: {
+                                __pos: (center - dist - 40) * scale,
+                            },
+                            onmousedown() {
+                                dragged = 'left';
+                                return false;
+                            },
+                        }),
+                        $._.right({
+                            style: {
+                                __pos: (center + dist + 40) * scale,
+                            },
+                            onmousedown() {
+                                dragged = 'right';
+                                return false;
+                            },
+                        }),
+                    )
                 );
             },
         };
